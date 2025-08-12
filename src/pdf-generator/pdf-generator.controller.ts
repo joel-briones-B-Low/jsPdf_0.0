7243d38f6d9a163870657a4f506ebf28b3e3a4b3
joel-briones-B-Low <joellbriones701@gmail.com>
2025-08-12 11:38:56 -0600

    implementation of puppeter for generate pdf

--- src/pdf-generator/pdf-generator.controller.ts
+++ src/pdf-generator/pdf-generator.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { PdfGeneratorService } from './pdf-generator.service';
+import { Controller, Get, Res, Param } from '@nestjs/common';
+import type { Response } from 'express';
+import { PdfGeneratorService } from '../pdf-generator/pdf-generator.service';
 
 @Controller('pdf-generator')
 export class PdfGeneratorController {
   constructor(private readonly pdfGeneratorService: PdfGeneratorService) {}
+
+   @Get('generate/:month')
+  async generatePdf(@Param('month') month: string, @Res() res: Response) {
+    const pdfBuffer = await this.pdfGeneratorService.generatePdf(month);
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename="document.pdf"',
+    });
+    res.send(pdfBuffer);
+  }
 }
